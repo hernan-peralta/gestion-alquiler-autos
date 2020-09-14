12416e8f8f24de74552613a55bd885796f40d012
Hernán Peralta <alegodemencia@gmail.com>
2020-09-13 21:25:21 -0300

    Agrega mapeador al modulo car

---
+++ src/module/car/mapper/carMapper.js
@@ -0,0 +1,64 @@
+const Car = require('../entity/car');
+
+/**
+ *
+ * @param {Object} formData
+ * @returns Car
+ */
+function fromDataToEntity({
+  id,
+  marca,
+  modelo,
+  año,
+  kms,
+  color,
+  'aire-acondicionado': aireAcondicionado,
+  pasajeros,
+  transmision,
+}) {
+  return new Car({
+    id,
+    marca,
+    modelo,
+    año,
+    kms,
+    color,
+    aireAcondicionado,
+    pasajeros,
+    transmision,
+  });
+}
+
+/**
+ *
+ * @param {Object} formData
+ * @returns Club
+ */
+function fromDbToEntity({
+  id,
+  marca,
+  modelo,
+  año,
+  kms,
+  color,
+  aire_acondicionado: aireAcondicionado,
+  pasajeros,
+  transmision,
+}) {
+  return new Car({
+    id,
+    marca,
+    modelo,
+    año,
+    kms,
+    color,
+    aireAcondicionado,
+    pasajeros,
+    transmision,
+  });
+}
+
+module.exports = {
+  fromDataToEntity,
+  fromDbToEntity,
+};
