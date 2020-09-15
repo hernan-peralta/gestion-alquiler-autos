abd4c05a70aeb09637c85d5c215224a4dd161fa2
Hernán Peralta <alegodemencia@gmail.com>
2020-09-15 00:23:15 -0300

    Agrega nuevas rutas al controlador

--- src/module/car/controller/carController.js
+++ src/module/car/controller/carController.js
@@ -1,10 +1,14 @@
+const { fromDataToEntity } = require('../mapper/carMapper');
+const CarIdNotDefinedError = require('./error/carIdNotDefinedError');
+
 module.exports = class CarController {
 /**
- * @param {carService} carService
+ * @param {import('../service/carService')} carService
  */
 
   constructor(carService) {
     this.ROUTE_BASE = '/cars';
+    this.carService = carService;
   }
 
   /**
@@ -15,6 +19,9 @@ module.exports = class CarController {
 
     app.get(`${ROUTE}`, this.index.bind(this));
     app.get(`${ROUTE}/new`, this.new.bind(this));
+    app.get(`${ROUTE}/view/:id`, this.view.bind(this));
+    app.post(`${ROUTE}/save`, this.save.bind(this));
+    app.get(`${ROUTE}/delete/:id`, this.delete.bind(this));
   }
 
   /**
@@ -22,7 +29,8 @@ module.exports = class CarController {
    * @param {import('express').Response} res
    */
   async index(req, res) {
-    res.render('car/view/index.html');
+    const cars = await this.carService.getAll();
+    res.render('car/view/index.html', { data: { cars } });
   }
 
   /**
@@ -31,5 +39,43 @@ module.exports = class CarController {
    */
   async new(req, res) {
     res.render('car/view/form.html');
+  }
+
+  /**
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   */
+  async view(req, res) {
+    const { id } = req.params;
+    if (!id) {
+      throw new CarIdNotDefinedError();
+    }
+
+    const car = await this.carService.getById(id);
+    res.render('car/view/form.html', { data: { car } });
+  }
+
+  /**
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   */
+  async save(req, res) {
+    const car = fromDataToEntity(req.body);
+    console.log("car en cotroller.save es", car)
+    await this.carService.save(car);
+
+    res.redirect('/cars');
+  }
+
+  /**
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   */
+  async delete(req, res) {
+    const { id } = req.params;
+    const car = await this.carService.getById(id);
+    await this.carService.delete(car);
+
+    res.redirect('/cars');
   }
 };
