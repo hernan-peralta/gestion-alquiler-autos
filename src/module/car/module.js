cd3be93dde6e7d2364636aa07a82e3df2cb2486b
Hernán Peralta <alegodemencia@gmail.com>
2020-09-15 00:26:05 -0300

    Actualiza modulo.js

--- src/module/car/module.js
+++ src/module/car/module.js
@@ -1,5 +1,9 @@
 const CarController = require('./controller/carController');
+const CarService = require('./service/carService');
+const CarRepository = require('./repository/sqlite/carRepository');
 
 module.exports = {
   CarController,
+  CarService,
+  CarRepository
 };
