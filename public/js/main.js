b4be14e1a4b777197eaf61ea1fa9d397cd552004
Hernán Peralta <alegodemencia@gmail.com>
2020-10-19 17:58:46 -0300

    Reorganiza templates y archivo publico js

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,8 @@
-document.querySelectorAll('.delete').forEach(elem => elem.addEventListener('click', (e) => {
+document.querySelectorAll('.delete').forEach((elem) => elem.addEventListener('click', (e) => {
   if (confirm('¿Está seguro de realizar esta acción? El resultado será irreversible')) {
     return true;
   }
-  else{
-    e.preventDefault();
-    return false;
-  }
+
+  e.preventDefault();
+  return false;
 }));
